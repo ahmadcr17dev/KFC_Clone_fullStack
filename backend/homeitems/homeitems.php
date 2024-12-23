16b80d371f4bac2750d3209e296c6bf9670f9517
ahmadcr17dev <chaudaryahmad1041@gmail.com>
2024-12-24 02:12:12 +0500

    Done

--- backend/homeitems/homeitems.php
+++ backend/homeitems/homeitems.php
@@ -2,29 +2,18 @@
 
 // Enable CORS
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Content-Type: application/json');
-
-// Respond to preflight option request
-// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-//     http_response_code(204);
-//     exit();
-// }
+header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Headers: X-Requested-With');
+// header('Content-Type: application/json');
 
 // Include database connection
 include('../database/connection.php');
 
 $sql_query = "SELECT * FROM home_items";
-$result = $conn->query($sql_query);
-$items = [];
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $items[] = $row;
-    }
-    echo json_encode($items);
-} else {
-    echo json_encode([]);
+$result = mysqli_query($conn, $sql_query);
+$items = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $items[] = $row;
 }
-
-$conn -> close();
+echo json_encode(['result'=>$result]);
+$conn->close();
