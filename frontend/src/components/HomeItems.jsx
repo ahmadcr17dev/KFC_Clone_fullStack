0120c75eb43a2e56ef83d07365d12950e8570697
ahmadcr17dev <chaudaryahmad1041@gmail.com>
2025-02-17 14:44:02 +0500

    Done

--- frontend/src/components/HomeItems.jsx
+++ frontend/src/components/HomeItems.jsx
@@ -7,7 +7,7 @@ const HomeItems = () => {
 
   useEffect(() => {
     axios
-      .get("https://kicksandfits.lovestoblog.com/api/homeitems.php")
+      .get("https://cors-anywhere.herokuapp.com/https://kicksandfits.lovestoblog.com/api/homeitems.php")
       .then((response) => {
         console.log("API Data:", response.data);
         setItems(Array.isArray(response.data) ? response.data : []);
