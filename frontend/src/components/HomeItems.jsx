25e5399dbe2d47dd9a641c16bd03cd60dde3164d
ahmadcr17dev <chaudaryahmad1041@gmail.com>
2025-02-18 14:43:57 +0500

    Done

--- frontend/src/components/HomeItems.jsx
+++ frontend/src/components/HomeItems.jsx
@@ -7,11 +7,11 @@ const HomeItems = () => {
 
   useEffect(() => {
     axios
-      .get(homeitems_key)
+      .get("http://kicksandfits.mygamesonline.org/homeitems.php")
       .then((response) => {
         console.log("API Data:", response.data);
         const itemsData = Array.isArray(response.data) ? response.data : [];
-      setItems(itemsData);
+        setItems(itemsData);
       })
       .catch((error) => console.error("Fetch error:", error));
   }, []);
@@ -25,26 +25,30 @@ const HomeItems = () => {
       <p className="text-red-600 font-bold text-left -mt-3">__________</p>
 
       {/* Products Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-10">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="bg-stone-800 rounded-lg shadow-md p-4 hover:cursor-pointer transition-transform hover:scale-105"
-          >
-            <p className="text-white font-semibold text-xl md:text-2xl">
-              {item.name}
-            </p>
-            <p className="text-white font-semibold bg-red-600 w-fit px-4 py-1 rounded-tr-xl rounded-bl-xl mt-2">
-              Rs: {item.price}
-            </p>
-            <img
-              src={`data:image/jpeg;base64,${item.image}`}
-              alt={item.name}
-              className="w-full h-48 md:h-56 lg:h-64 object-cover rounded-md mt-4"
-            />
-          </div>
-        ))}
-      </div>
+      {items.length === 0 ? (
+        <p>No item to show</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-10">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="bg-stone-800 rounded-lg shadow-md p-4 hover:cursor-pointer transition-transform hover:scale-105"
+            >
+              <p className="text-white font-semibold text-xl md:text-2xl">
+                {item.name}
+              </p>
+              <p className="text-white font-semibold bg-red-600 w-fit px-4 py-1 rounded-tr-xl rounded-bl-xl mt-2">
+                Rs: {item.price}
+              </p>
+              <img
+                src={`data:image/jpeg;base64,${item.image}`}
+                alt={item.name}
+                className="w-full h-48 md:h-56 lg:h-64 object-cover rounded-md mt-4"
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
